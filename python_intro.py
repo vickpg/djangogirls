af73fb90a2dc4bb9ee234efcd55931629e4dfd1a
vickpg <pgomesvictoria@gmail.com>
2022-05-11 11:30:20 -0300

    python tutorial

--- python_intro.py
+++ python_intro.py
@@ -1,1 +1,29 @@
-print ("Hello World!")
+#print ("Hello World!")
+
+#if 3 > 2:
+    #print ("Funciona!")
+
+#name = 'Vick'
+#if name == 'Ola':
+    #print('Olá Ola!')
+#elif name == 'Vick':
+    #print('Olá Vick!')
+#else:
+    #print('Olá estranho!')
+
+#def hi():
+    #print('Olá!')
+    #print('Tudo bem?')
+
+#hi()
+
+#def hi(name):
+    #print('Olá ' + name + '!')
+
+#girls = ['Rachel', 'Monica', 'Phoebe']
+#for name in girls:
+    #hi(name)
+    #print('Próxima')
+
+for i in range(1, 8):
+    print(i)
