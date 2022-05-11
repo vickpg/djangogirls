0d83699a74b53997e299322c32cc78ec52514a29
vickpg <pgomesvictoria@gmail.com>
2022-05-11 16:34:56 -0300

    test new class atribute

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    test = models.TextField(null=True)
 
     def publish(self):
         self.published_date = timezone.now()
