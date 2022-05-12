e35fc09d1bbdba3b293130483fe4cfe6022c55e8
vickpg <pgomesvictoria@gmail.com>
2022-05-12 12:56:52 -0300

    fix error

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-Post.objects.get(pk=pk)
-
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
