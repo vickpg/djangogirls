d41ff09bc197f1d65dab6007a9d4822d806ed056
vickpg <pgomesvictoria@gmail.com>
2022-05-12 15:08:29 -0300

    correct def post_new error

--- blog/views.py
+++ blog/views.py
@@ -15,9 +15,6 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
-
     if request.method == "POST":
         form = PostForm(request.POST)
     else:
