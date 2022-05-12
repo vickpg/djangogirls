60a454583295cefd346ff54c6b3b06f16e524675
vickpg <pgomesvictoria@gmail.com>
2022-05-12 09:53:49 -0300

    add QuerySet (posts)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # função de solicitação e que irá retornar valor que recebe ao chamar outra função render (renderizar) modelo blog 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
