0af21b7765507d6cba72de8ff8d7defdc411beed
vickpg <pgomesvictoria@gmail.com>
2022-05-12 13:11:55 -0300

    add new view and template form

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 # função de solicitação e que irá retornar valor que recebe ao chamar outra função render (renderizar) modelo blog 
 def post_list(request):
@@ -11,3 +12,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
